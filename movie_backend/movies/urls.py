6432ab772563551e83019634d032072f6f1f26ba
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-03-06 14:43:25 +0300

    Adding api

--- movie_backend/movies/urls.py
+++ movie_backend/movies/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, TrendingMoviesView, RecommendedMoviesView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('movies/trending/', TrendingMoviesView.as_view(), name='trending_movies'),
+    path('movies/recommended/', RecommendedMoviesView.as_view(), name='recommended_movies'),
 ]
--- movie_backend/movies/views.py
+++ movie_backend/movies/views.py
@@ -1,8 +1,32 @@
+import requests
 from rest_framework import generics
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.serializers import ModelSerializer
+
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+TMDB_API_KEY = "4931b9c8c2283aea09990b37a2ac2f15"
+
+class TrendingMoviesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        url = f"https://api.themoviedb.org/3/trending/movie/week?api_key={TMDB_API_KEY}"
+        response = requests.get(url)
+        return Response(response.json())
+
+class RecommendedMoviesView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}"
+        response = requests.get(url)
+        return Response(response.json())
 
 User = get_user_model()
 
