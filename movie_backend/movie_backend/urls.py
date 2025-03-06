aedadb418ab3b1c3daef89da5026e68e821e753e
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-03-06 08:25:22 +0300

    ensuring the api routing works

--- movie_backend/movie_backend/urls.py
+++ movie_backend/movie_backend/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
+def home_view(request):
+    return JsonResponse({"message": "Welcome to the Movie Recommendation API!"})
+
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('movies.urls')),
     
