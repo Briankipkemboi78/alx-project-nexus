82dde3474b99eaaecec36768d9873ebbdbde7b02
Briankipkemboi78 <briankipkemboi47@gmail.com>
2025-03-06 08:12:41 +0300

    Adding serializer and regirtration views

--- movie_backend/movies/views.py
+++ movie_backend/movies/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.serializers import ModelSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save(password=self.request.data['password'])
